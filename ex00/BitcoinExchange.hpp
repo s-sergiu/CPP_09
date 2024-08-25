86244844c4981c2b93e0e5f02d3c212b2019c8c8
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-08-25 17:45:43 +0200

    implemented header file function prototypes

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -4,6 +4,11 @@
 
 class BitcoinExchange {
 	public:
+		BitcoinExchange(void);
+		BitcoinExchange(const BitcoinExchange &src);
+		BitcoinExchange operator = (const BitcoinExchange &src);
+		~BitcoinExchange(void);
+		void parse(char **argv);
 };
 
 #endif
