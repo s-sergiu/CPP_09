17b2d0433dcffb39eb69e4657a9642cf515df893
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-08-25 17:46:04 +0200

    update main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,7 +1,19 @@
 
 #include "BitcoinExchange.hpp"
+#include <iostream>
+#include <exception>
 
-int main(void) {
-
+int main(int argc, char **argv) {
+	(void)argv;
+	BitcoinExchange obj;
+	if (argc == 2)	
+		try {
+			obj.parse(argv);	
+		} catch(std::exception &e) {
+			std::cerr<<"Error: "<<e.what()<<std::endl;
+		}
+	else {
+		std::cerr<<"Invalid number of arguments!"<<std::endl;
+	}
 	return 0;
 }
