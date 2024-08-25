ab1a29bfb8aa7f0c39705000c1f569dcf1ebc375
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-08-25 17:45:23 +0200

    implemented empty constructors and parse function

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -1,2 +1,23 @@
 
 #include "BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange(void) {
+
+}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &src) {
+	(void)src;
+}
+
+BitcoinExchange BitcoinExchange::operator = (const BitcoinExchange &src) {
+	(void)src;
+	return (*this);
+}
+
+BitcoinExchange::~BitcoinExchange(void) {
+
+}
+
+void BitcoinExchange::parse(char **argv) {
+	(void)argv;
+}
