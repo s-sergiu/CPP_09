8a35ee9427f450bb7499ca34e2d935bddd56f8d4
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-08-26 17:58:24 +0200

    Implemented checkFileHeader

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -29,7 +29,34 @@ void BitcoinExchange::parse(char *argv) {
 	file.open(argv, std::fstream::in);
 
 	if (file.is_open())
-		checkFileFormatting(file);	
+		this->checkFileFormatting(file);	
 	else 
 		throw std::runtime_error("Could not open file for reading!");
+}
+
+void BitcoinExchange::checkFileFormatting(std::fstream &file) const {
+
+	std::string str;
+
+	checkFileHeader(file);
+	while(std::getline(file, str))
+			checkFileValues(str);
+}
+
+void BitcoinExchange::checkFileHeader(std::fstream &file) const {
+	std::string str;
+	file >> str;
+	if (str != "date")
+		throw std::runtime_error("Incorrect header format for input file");
+	file >> str;
+	if (str != "|")
+		throw std::runtime_error("Incorrect header format for input file");
+	file >> str;
+	if (str != "value")
+		throw std::runtime_error("Incorrect header format for input file");
+}
+
+void BitcoinExchange::checkFileValues(std::string &str) const {
+	if (!str.empty())
+		std::cout<<"line: "<<str<<std::endl;
 }
--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -3,16 +3,20 @@
 #define BITCOIN_HPP
 
 #include <stdexcept>
+#include <set>
 #include <fstream>
 
 class BitcoinExchange {
+		std::set<int> input;
 	private:
 		BitcoinExchange(const BitcoinExchange &);
 		BitcoinExchange &operator = (const BitcoinExchange &);
-		~BitcoinExchange(void);
 		void checkFileFormatting(std::fstream &) const;
+		void checkFileHeader(std::fstream &) const;
+		void checkFileValues(std::string &) const;
 	public:
 		BitcoinExchange(void);
+		~BitcoinExchange(void);
 		void parse(char *);
 };
 
