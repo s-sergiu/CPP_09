82c3e47ee05d4243e3c4222ec597d8f9b1c4fd93
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-08-30 06:27:54 +0200

    refactored loadStack, created handleOperator

--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -8,14 +8,21 @@
 #include <sstream>
 #include <cstdlib>
 
+#define ADD +
+#define MIN -
+#define DIV /
+#define MUL *
+
 class RPN {
 	private:
+		int buffer;
 		std::string args;
 		std::stack<int> stack_a;
 		int getNumber(std::string &str) const;
 		char getOperand(std::string &str) const;
 		void IllegalSymbolCheck(void);
 		void loadStack(void);
+		void handleOperator(char);
 	public:
 		void parse(char *);
 };
