b42d0399e9c6f28749fcc2417636d9d78a2bacff
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-08-30 05:19:55 +0200

    implemented main file

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,6 +1,16 @@
 #include "RPN.hpp"
 
 int main(int argc, char **argv) {
+	RPN obj;
 
+	if (argc == 2) {
+		try {
+			obj.parse(argv[1]);
+		} catch (std::exception &e) {
+			std::cerr<<"Exception: "<<e.what()<<std::endl;
+		}
+	}
+	else
+		std::cerr<<"Usage: ./RPN \"8 9 * 9 - 9 - 4 - 1 +\""<<std::endl;
 	return 0;
 }
