2c7028e49c7ed415ceae6a4e5931f27151979a4a
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-09-01 23:17:23 +0200

    implemented merge sort

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -8,8 +8,7 @@ PmergeMe::PmergeMe(void) {
 	last = -1;
 }
 
-void PmergeMe::printPairedList(std::vector<std::pair<int, int> > &list, std::string msg) {
-	std::cout<<std::endl;
+void PmergeMe::printPairedList(std::vector<std::pair<int, int> > &list, std::string msg, int level) {
 	std::cout<<"\e[44m-------------------------------\e[0m"<<std::endl;
 	std::cout<<"\e[102m"<<msg<<"\e[0m"<<std::endl;
 	std::cout<<"\e[44m--------------size-------------\e[0m"<<std::endl;
@@ -18,9 +17,12 @@ void PmergeMe::printPairedList(std::vector<std::pair<int, int> > &list, std::str
 
 	std::cout<<"\e[44m-------------begin-------------\e[0m"<<std::endl;
 	for(it = list.begin(); it != list.end(); ++it) {
-		std::cout<<it->first<<" ";
-		std::cout<<it->second<<" ";
-		std::cout<<"| ";
+		if (level == 0 || level == 1)
+			std::cout<<it->first<<" ";
+		if (level == 0 || level == 2)
+			std::cout<<it->second<<" ";
+		if (level == 0)
+			std::cout<<"| ";
 	}
 	if (list.empty())
 		std::cout<<"\e[31mList is empty!\e[0m";
@@ -32,7 +34,6 @@ void PmergeMe::printPairedList(std::vector<std::pair<int, int> > &list, std::str
 }
 
 void PmergeMe::printList(std::vector<int> &list, std::string msg) {
-	std::cout<<std::endl;
 	std::cout<<"\e[45m-------------------------------\e[0m"<<std::endl;
 	std::cout<<"\e[102m"<<msg<<"\e[0m"<<std::endl;
 	std::cout<<"\e[45m--------------size-------------\e[0m"<<std::endl;
@@ -60,10 +61,15 @@ void PmergeMe::parse(char **arg, int argc) {
 	}
 	printList(list, "initial");
 	createPairs();
-	sortPairs();
+	sortSinglePairs();
 	printList(list, "after created and sort pairs");
-	printPairedList(pairs, "after created and sort pairs");
+	printPairedList(pairs, "after created and sort pairs", 0);
+	mergeSortPair(pairs, 0, pairs.size() - 1);
+	printPairedList(pairs, "after merge sort" , 0);
 	insertLowerSort();
+	printList(list, "list");
+	printList(sublist, "sublist");
+	//insertLowerSort();
 }
 
 void PmergeMe::illegalSymbolCheck(void) {
@@ -119,7 +125,7 @@ void PmergeMe::createPairs(void) {
 		extractPairFromOddList();
 }
 
-void PmergeMe::sortPairs(void) {
+void PmergeMe::sortSinglePairs(void) {
 	std::vector<std::pair<int, int> >::iterator it;
 	int temp;
 
@@ -130,6 +136,19 @@ void PmergeMe::sortPairs(void) {
 			it->first = temp;
 		}
 	}
+}
+
+std::vector<std::pair<int, int> >::iterator PmergeMe::getMaxValue(void) {
+	std::vector<std::pair<int, int> >::iterator max;
+	std::vector<std::pair<int, int> >::iterator it;
+
+	max = pairs.begin();
+	for(it = pairs.begin(); it != pairs.end(); ++it) {
+		if (it->first < max->first)
+			max = it;
+	}
+
+	return max;
 }
 
 std::vector<std::pair<int, int> >::iterator PmergeMe::getMinValue(void) {
@@ -150,10 +169,71 @@ void PmergeMe::insertLowerSort(void) {
 	std::vector<std::pair<int, int> >::iterator min;
 
 	while (!pairs.empty()) {
-		min = getMinValue();
+		min = getMaxValue();
 		list.push_back(min->first);
 		sublist.push_back(min->second);
 		pairs.erase(min);
 	}
+	if (last != -1)
+		sublist.push_back(last);
 }
 
+void PmergeMe::mergeSort() {
+	int len;
+	std::vector<std::pair<int, int> > temp;
+	std::vector<std::pair<int, int> >::iterator it;
+
+	len = pairs.size() / 2;
+	if (pairs.size() > 1)
+		std::cout<<"we can mergesort"<<std::endl;
+
+	std::cout<<"half len is: "<<len<<std::endl;
+	if (pairs.begin() > pairs.end()) {
+		temp.push_back(pairs.back());
+		temp.push_back(pairs.front());
+	}
+	else {
+		temp.push_back(pairs.front());
+		temp.push_back(pairs.back());
+	}
+	printPairedList(temp, "temp", 0);
+}
+
+void PmergeMe::merge(std::vector<std::pair<int, int> > &arr, int start, int middle, int end) {
+
+    std::vector<std::pair<int, int> > leftArray(middle - start + 1);
+    std::vector<std::pair<int, int> > rightArray(end - middle);
+    unsigned int l = 0;
+	unsigned int r = 0;
+    unsigned int i = start;
+
+    for (unsigned int i = 0; i < leftArray.size(); ++i)
+        leftArray[i] = arr[start + i];
+    for (unsigned int i = 0; i < rightArray.size(); ++i)
+        rightArray[i] = arr[middle + 1 + i];
+
+    while (l < leftArray.size() && r < rightArray.size()) {
+        if (leftArray[l] <= rightArray[r]) {
+            arr[i] = leftArray[l];
+            l++;
+        } else {
+            arr[i] = rightArray[i];
+            r++;
+        }
+        i++;
+    }
+    while (l < leftArray.size())
+		arr[i++] = leftArray[l++];
+    while (r < rightArray.size())
+		arr[i++] = rightArray[r++];
+}
+
+void PmergeMe::mergeSortPair(std::vector<std::pair<int ,int> > &vec, int start, int end)
+{
+	if (start < end) {
+		int middle = (start + end) / 2;
+		mergeSortPair(vec, start, middle);
+		mergeSortPair(vec, middle + 1, end);
+		merge(vec, start, middle, end);
+	}
+}
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -19,11 +19,15 @@ class PmergeMe {
 		void createPairs(void);
 		void extractPairFromOddList(void);
 		void extractPairFromEvenList(void);
-		void sortPairs(void);
+		void sortSinglePairs(void);
+		void mergeSort(void);
+		void merge(std::vector<std::pair<int, int> > &, int, int, int);
+		void mergeSortPair(std::vector<std::pair<int ,int> > &, int, int);
 		void insertLowerSort(void);
 		std::vector<std::pair<int, int> >::iterator getMinValue(void);
+		std::vector<std::pair<int, int> >::iterator getMaxValue(void);
 		void printList(std::vector<int> &, std::string);
-		void printPairedList(std::vector<std::pair<int, int> > &, std::string);
+		void printPairedList(std::vector<std::pair<int, int> > &, std::string, int);
 	public:
 		PmergeMe(void);
 		void parse(char**, int);
